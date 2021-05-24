8e4c713a1e1a4aa7a57ebcca1dd16f4c243b3c54
tamara.stevanovic <tamara.stevanovic@LiveNation.com>
2021-05-24 21:19:00 +0200

    dodata forma za unos komentara

--- src/app/pages/movie/movie/movie.component.ts
+++ src/app/pages/movie/movie/movie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AppService} from '../../../core/app.service';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-movie',
@@ -9,24 +10,43 @@ import {AppService} from '../../../core/app.service';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
-  movie = '';
-  public commentForm = new FormGroup({
-    comment: new FormControl(),
-  });
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private appService: AppService,
   ) {}
+  movie = '';
+  public commentForm = new FormGroup({
+    comment: new FormControl(),
+  });
+
+  newComment = '';
 
   ngOnInit(): void {
     this.movie =  JSON.parse(window.sessionStorage.getItem('movieToShow'));
     this.commentForm.reset();
   }
-  commentMovie(){
-    //create call to movie service and add comment to DB
-    this.commentForm.reset();
+
+  commentMovie(newComment){
+    if (this.commentForm.invalid) {
+      return;
+    }
+    // create call to movie service and add comment to DB
+    this.newComment = newComment.toString();
+
+    const body = new HttpParams()
+      .set('comment', this.newComment);
+
+    this.appService.giveComment(body.toString()).subscribe(
+      (data: any) => {
+        window.sessionStorage.setItem('comm', JSON.stringify(data));
+      },
+      (error: { error: { error_description: any } }) => {
+        alert(error.error.error_description);
+      }
+    );
+
     alert('clicked me!');
   }
 
