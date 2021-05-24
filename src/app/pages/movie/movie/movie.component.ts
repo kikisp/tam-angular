d13d321d169063097b1d7da345dd5a923552cd6e
tamara.stevanovic <tamara.stevanovic@LiveNation.com>
2021-05-24 15:19:05 +0200

    zapoceta implementacija fje commentMovie()

--- src/app/pages/movie/movie/movie.component.ts
+++ src/app/pages/movie/movie/movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AppService} from '../../../core/app.service';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  movie = '';
+  public commentForm = new FormGroup({
+    comment: new FormControl(),
+  });
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private appService: AppService,
+  ) {}
 
   ngOnInit(): void {
+    this.movie =  JSON.parse(window.sessionStorage.getItem('movieToShow'));
+    this.commentForm.reset();
+  }
+  commentMovie(){
+    //create call to movie service and add comment to DB
+    this.commentForm.reset();
+    alert('clicked me!');
   }
 
 }
