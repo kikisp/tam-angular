193f0945d074268ad5a3665d686d8fa71f5a53c8
Stefan Petrovic <kikisp1@gmail.com>
2021-09-01 14:14:08 +0200

    added parsing of backend data

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {AppService} from '../../core/app.service';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {HttpParams} from '@angular/common/http';
+import { AppService } from '../../core/app.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
 
 export class RoleUsername {
   username: string;
@@ -11,7 +16,7 @@ export class RoleUsername {
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
   public welcomeForm = new FormGroup({
@@ -37,9 +42,9 @@ export class WelcomeComponent implements OnInit {
     });
     this.appService.getUser().subscribe(
       (data: any) => {
-        this.namerole = data;
-        console.log("DATA  "+ data);
-        sessionStorage.setItem('username', this.name);
+        sessionStorage.setItem('username', data.username);
+        sessionStorage.setItem('role', data.role);
+        if ()
       },
       (error: { error: { error_description: any } }) => {
         alert(error.error.error_description);
@@ -48,26 +53,31 @@ export class WelcomeComponent implements OnInit {
   }
 
   onSubmit() {
-   if (this.welcomeForm.invalid) {
+    if (this.welcomeForm.invalid) {
       return;
     }
-   const body = new HttpParams()
+    const body = new HttpParams()
       .set('movie', this.welcomeForm.controls.title.value)
       .set('year', this.welcomeForm.controls.year.value)
       .set('grant_type', 'password');
-   this.appService.searchMovies(this.welcomeForm.controls.title.value, this.welcomeForm.controls.year.value).subscribe(
-      (data: any) => {
-        this.movie = data;
-        window.sessionStorage.setItem('movieToShow', JSON.stringify(data));
+    this.appService
+      .searchMovies(
+        this.welcomeForm.controls.title.value,
+        this.welcomeForm.controls.year.value
+      )
+      .subscribe(
+        (data: any) => {
+          this.movie = data;
+          window.sessionStorage.setItem('movieToShow', JSON.stringify(data));
 
-        this.items = new Map()
-          .set('YEAR', data.Year)
-          .set('TITLE', data.Title);
-      },
-      (error: { error: { error_description: any } }) => {
-        alert(error.error.error_description);
-      }
-    );
+          this.items = new Map()
+            .set('YEAR', data.Year)
+            .set('TITLE', data.Title);
+        },
+        (error: { error: { error_description: any } }) => {
+          alert(error.error.error_description);
+        }
+      );
   }
 
   clickFunction() {
@@ -75,6 +85,4 @@ export class WelcomeComponent implements OnInit {
     this.router.navigate(['movie']);
     // alert('clicked me!');
   }
-
-
 }
